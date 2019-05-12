ae0fea573a1063451730bbfcbdee248df545d58b
AmrDeveloper <Amr96@programmer.net>
2019-05-12 21:50:12 +0200

    Add Profile menu to MainActivity

--- app/src/main/java/com/amrdeveloper/gitecho/view/MainActivity.java
+++ app/src/main/java/com/amrdeveloper/gitecho/view/MainActivity.java
@@ -3,6 +3,9 @@ package com.amrdeveloper.gitecho.view;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.amrdeveloper.gitecho.R;
 import com.amrdeveloper.gitecho.databinding.ActivityMainBinding;
@@ -15,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuId = item.getItemId();
+        if (menuId == R.id.profileMenu) {
+            //TODO : Go To ProfileActivity
+            Toast.makeText(this, "Open Profile Activity", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
