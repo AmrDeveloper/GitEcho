a73c9bc5f977c88ccbbf8f9d6231a6bf766305c6
AmrDeveloper <Amr96@programmer.net>
2019-05-12 21:23:49 +0200

    add DataBinding to MainActivity

--- app/src/main/java/com/amrdeveloper/gitecho/view/MainActivity.java
+++ app/src/main/java/com/amrdeveloper/gitecho/view/MainActivity.java
@@ -1,15 +1,19 @@
 package com.amrdeveloper.gitecho.view;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.amrdeveloper.gitecho.R;
+import com.amrdeveloper.gitecho.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 }
