010072673d575e471c29e04aecc2bf0680d65300
AmrDeveloper <Amr96@programmer.net>
2019-05-18 02:56:00 +0200

    Finish StarsPresenter

--- app/src/main/java/com/amrdeveloper/gitecho/model/StarsModel.java
+++ app/src/main/java/com/amrdeveloper/gitecho/model/StarsModel.java
@@ -1,4 +1,16 @@
 package com.amrdeveloper.gitecho.model;
 
-public class StarsModel {
+import android.arch.lifecycle.LifecycleOwner;
+
+import com.amrdeveloper.gitecho.model.contract.StarsContract;
+import com.amrdeveloper.gitecho.model.listener.OnLoadListener;
+import com.amrdeveloper.gitecho.model.network.stars.StarsViewModel;
+
+public class StarsModel implements StarsContract.Model {
+
+    @Override
+    public void loadingDataFromApi(StarsViewModel viewModel, LifecycleOwner owner, OnLoadListener listener) {
+
+    }
+
 }
--- app/src/main/java/com/amrdeveloper/gitecho/presenter/StarsPresenter.java
+++ app/src/main/java/com/amrdeveloper/gitecho/presenter/StarsPresenter.java
@@ -1,5 +1,39 @@
 package com.amrdeveloper.gitecho.presenter;
 
-public class StarsPresenter {
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.paging.PagedList;
 
+import com.amrdeveloper.gitecho.model.StarsModel;
+import com.amrdeveloper.gitecho.model.contract.StarsContract;
+import com.amrdeveloper.gitecho.model.listener.OnLoadListener;
+import com.amrdeveloper.gitecho.model.network.stars.StarsViewModel;
+import com.amrdeveloper.gitecho.object.Stargazer;
+
+public class StarsPresenter
+        implements StarsContract.Presenter
+        , OnLoadListener<PagedList<Stargazer>> {
+
+    private StarsContract.View view;
+    private StarsContract.Model model;
+    private StarsViewModel viewModel;
+    private LifecycleOwner lifecycleOwner;
+
+    public StarsPresenter(LifecycleOwner lifecycleOwner, StarsViewModel viewModel, StarsContract.View view){
+        this.view = view;
+        this.viewModel = viewModel;
+        this.lifecycleOwner = lifecycleOwner;
+        this.model = new StarsModel();
+    }
+
+    @Override
+    public void startLoadingData() {
+        view.showProgressBar();
+        model.loadingDataFromApi(viewModel,lifecycleOwner,this);
+    }
+
+    @Override
+    public void onLoadFinish(PagedList<Stargazer> result) {
+        view.onLoadFinish(result);
+        view.hideProgressBar();
+    }
 }
