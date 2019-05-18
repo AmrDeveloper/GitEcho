5fc9f64830e7cf52ca5a9b0d2447ddef290452c6
AmrDeveloper <Amr96@programmer.net>
2019-05-18 03:11:15 +0200

    Finish StarsDataSource

--- app/src/main/java/com/amrdeveloper/gitecho/model/network/stars/StarsViewModel.java
+++ app/src/main/java/com/amrdeveloper/gitecho/model/network/stars/StarsViewModel.java
@@ -1,4 +1,43 @@
 package com.amrdeveloper.gitecho.model.network.stars;
 
-public class StarsViewModel {
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import android.arch.paging.LivePagedListBuilder;
+import android.arch.paging.PageKeyedDataSource;
+import android.arch.paging.PagedList;
+
+import com.amrdeveloper.gitecho.object.Stargazer;
+
+public class StarsViewModel extends ViewModel {
+
+    private static String sUsername;
+    private static String sRepoFullName;
+    private LiveData<PagedList<Stargazer>> itemPagedList;
+    private LiveData<PageKeyedDataSource<Integer, Stargazer>> liveDataSource;
+
+    public StarsViewModel(){
+        StarsDataSoruceFactory itemDataSourceFactory = new StarsDataSoruceFactory(sUsername,sRepoFullName);
+        liveDataSource = itemDataSourceFactory.getStarsLiveDataSource();
+
+        PagedList.Config config =
+                (new PagedList.Config.Builder())
+                        .setEnablePlaceholders(false)
+                        .setPageSize(StarsDataSource.PAGE_SIZE)
+                        .build();
+
+        itemPagedList = (new LivePagedListBuilder<>(itemDataSourceFactory, config)).build();
+    }
+
+    public LiveData<PagedList<Stargazer>> getItemPagedList() {
+        return itemPagedList;
+    }
+
+    public LiveData<PageKeyedDataSource<Integer, Stargazer>> getLiveDataSource() {
+        return liveDataSource;
+    }
+
+    public static void setRequestData(String username,String repoName){
+        sUsername = username;
+        sRepoFullName = repoName;
+    }
 }
