7c7579f125e4e648ca51949f2bca3f1f7ce53582
AmrDeveloper <Amr96@programmer.net>
2019-05-20 18:07:29 +0200

    Beta design of RepositoryActivity

---
+++ app/src/main/java/com/amrdeveloper/gitecho/view/RepositoryActivity.java
@@ -0,0 +1,15 @@
+package com.amrdeveloper.gitecho.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amrdeveloper.gitecho.R;
+
+public class RepositoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository);
+    }
+}
