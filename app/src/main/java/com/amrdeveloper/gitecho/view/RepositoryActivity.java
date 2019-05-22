7db9e1ca65cfc3919a2ab6cfab9e3bf714d87413
AmrDeveloper <Amr96@programmer.net>
2019-05-22 12:55:28 +0200

    Add SVG icon for License

--- app/src/main/java/com/amrdeveloper/gitecho/view/RepositoryActivity.java
+++ app/src/main/java/com/amrdeveloper/gitecho/view/RepositoryActivity.java
@@ -1,15 +1,19 @@
 package com.amrdeveloper.gitecho.view;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.amrdeveloper.gitecho.R;
+import com.amrdeveloper.gitecho.databinding.ActivityRepositoryBinding;
 
 public class RepositoryActivity extends AppCompatActivity {
+
+    private ActivityRepositoryBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_repository);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_repository);
     }
 }
