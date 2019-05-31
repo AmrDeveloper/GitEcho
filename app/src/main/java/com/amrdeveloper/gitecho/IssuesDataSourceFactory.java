882a23d80385726a361f468c62d3f16b50ba91e0
AmrDeveloper <Amr96@programmer.net>
2019-05-31 08:03:58 +0200

    Finish IssuesDataSoruceFactory

--- app/src/main/java/com/amrdeveloper/gitecho/IssuesDataSourceFactory.java
+++ app/src/main/java/com/amrdeveloper/gitecho/IssuesDataSourceFactory.java
@@ -1,4 +1,34 @@
 package com.amrdeveloper.gitecho;
 
-public class IssuesDataSourceFactory {
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.paging.DataSource;
+import android.arch.paging.PageKeyedDataSource;
+
+import com.amrdeveloper.gitecho.object.Issue;
+
+public class IssuesDataSourceFactory extends DataSource.Factory {
+
+    private String username;
+    private String repositoryName;
+    private String issueType;
+
+    private MutableLiveData<PageKeyedDataSource<Integer, Issue>> issuesLiveDataSource = new MutableLiveData<>();
+
+
+    public IssuesDataSourceFactory(String username, String repositoryName, String issueType) {
+        this.username = username;
+        this.repositoryName = repositoryName;
+        this.issueType = issueType;
+    }
+
+    @Override
+    public DataSource create() {
+        IssuesDataSource itemDataSource = new IssuesDataSource(username,repositoryName,issueType);
+        issuesLiveDataSource.postValue(itemDataSource);
+        return itemDataSource;
+    }
+
+    public MutableLiveData<PageKeyedDataSource<Integer, Issue>> getIssuesLiveDataSource() {
+        return issuesLiveDataSource;
+    }
 }
