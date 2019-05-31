2d69242d388bdee35f563aaa65847158981267fd
AmrDeveloper <Amr96@programmer.net>
2019-05-31 08:07:19 +0200

    Finish IssuesViewModel class now Issues ready for Paging feature

--- app/src/main/java/com/amrdeveloper/gitecho/IssuesViewModel.java
+++ app/src/main/java/com/amrdeveloper/gitecho/IssuesViewModel.java
@@ -1,4 +1,47 @@
 package com.amrdeveloper.gitecho;
 
-public class IssuesViewModel {
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import android.arch.paging.LivePagedListBuilder;
+import android.arch.paging.PageKeyedDataSource;
+import android.arch.paging.PagedList;
+
+import com.amrdeveloper.gitecho.object.Issue;
+
+public class IssuesViewModel extends ViewModel {
+
+    private static String sUsername;
+    private static String sRepoName;
+    private static String sIssueType;
+
+    private LiveData<PagedList<Issue>> itemPagedList;
+    private LiveData<PageKeyedDataSource<Integer, Issue>> liveDataSource;
+
+    public IssuesViewModel(){
+        IssuesDataSourceFactory itemDataSourceFactory = new IssuesDataSourceFactory(sUsername,sRepoName,sIssueType);
+        liveDataSource = itemDataSourceFactory.getIssuesLiveDataSource();
+
+        PagedList.Config config =
+                (new PagedList.Config.Builder())
+                        .setEnablePlaceholders(false)
+                        .setPageSize(IssuesDataSource.PAGE_SIZE)
+                        .setInitialLoadSizeHint(IssuesDataSource.INITIAL_LOAD_SIZE)
+                        .build();
+
+        itemPagedList = (new LivePagedListBuilder<>(itemDataSourceFactory, config)).build();
+    }
+
+    public LiveData<PagedList<Issue>> getItemPagedList() {
+        return itemPagedList;
+    }
+
+    public LiveData<PageKeyedDataSource<Integer, Issue>> getLiveDataSource() {
+        return liveDataSource;
+    }
+
+    public static void setRequestData(String username,String reponame,String issueType){
+        sUsername = username;
+        sRepoName = reponame;
+        sIssueType = issueType;
+    }
 }
