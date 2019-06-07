a123acda0991d4424795859be215bfa6fc141370
AmrDeveloper <Amr96@programmer.net>
2019-06-07 13:48:07 +0200

    use power of include tag to make generic list layout

--- app/src/main/java/com/amrdeveloper/gitecho/view/OpenRequestFragment.java
+++ app/src/main/java/com/amrdeveloper/gitecho/view/OpenRequestFragment.java
@@ -11,12 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amrdeveloper.gitecho.databinding.DataListViewBinding;
 import com.amrdeveloper.gitecho.model.contract.PullRequestContract;
 import com.amrdeveloper.gitecho.model.events.LoadFinishEvent;
 import com.amrdeveloper.gitecho.presenter.PullRequestPresenter;
 import com.amrdeveloper.gitecho.model.network.pulls.PullRequestViewModel;
 import com.amrdeveloper.gitecho.adapter.PullPagedAdapter;
-import com.amrdeveloper.gitecho.databinding.RequestListBinding;
 import com.amrdeveloper.gitecho.object.PullRequest;
 import com.amrdeveloper.gitecho.utils.Consts;
 
@@ -31,13 +31,13 @@ public class OpenRequestFragment
     private String username;
     private String repositoryName;
     private PullRequestPresenter presenter;
-    private RequestListBinding binding;
+    private DataListViewBinding binding;
     private PullPagedAdapter pullPagedAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = RequestListBinding.inflate(inflater,container,false);
+        binding = DataListViewBinding.inflate(inflater,container,false);
         setRecyclerViewSettings();
 
         PullRequestViewModel.setRequestData(username,repositoryName, PullRequest.STATE_OPEN);
@@ -51,9 +51,9 @@ public class OpenRequestFragment
 
     private void setRecyclerViewSettings() {
         pullPagedAdapter = new PullPagedAdapter(getActivity());
-        binding.requestRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        binding.requestRecyclerView.setHasFixedSize(true);
-        binding.requestRecyclerView.setAdapter(pullPagedAdapter);
+        binding.dataRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.dataRecyclerView.setHasFixedSize(true);
+        binding.dataRecyclerView.setAdapter(pullPagedAdapter);
     }
 
     @Override
